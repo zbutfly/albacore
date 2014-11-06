0e22f06266ccfe727b7212db43e593f2ae11b0f2
कलविङ्क <zhangx@hzcominfo.com>
2014-11-06 09:37:00 +0000

    httpclient version for bug.

--- utils/src/main/java/net/butfly/albacore/utils/http/HTTPAsyncUtils.java
+++ utils/src/main/java/net/butfly/albacore/utils/http/HTTPAsyncUtils.java
@@ -1,26 +1,26 @@
-package net.butfly.albacore.utils.http;
-
-import net.butfly.albacore.exception.SystemException;
-import net.butfly.albacore.utils.UtilsBase;
-
-import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
-import org.apache.http.impl.nio.client.HttpAsyncClients;
-import org.apache.http.impl.nio.conn.PoolingNHttpClientConnectionManager;
-import org.apache.http.impl.nio.reactor.DefaultConnectingIOReactor;
-import org.apache.http.nio.conn.NHttpClientConnectionManager;
-import org.apache.http.nio.reactor.IOReactorException;
-
-public class HTTPAsyncUtils extends UtilsBase {
-	private static NHttpClientConnectionManager asyncman;
-	static {
-		try {
-			asyncman = new PoolingNHttpClientConnectionManager(new DefaultConnectingIOReactor());
-		} catch (IOReactorException e) {
-			throw new SystemException("", e);
-		}
-	}
-
-	public static CloseableHttpAsyncClient create() {
-		return HttpAsyncClients.createMinimal(asyncman);
-	}
-}
+//package net.butfly.albacore.utils.http;
+//
+//import net.butfly.albacore.exception.SystemException;
+//import net.butfly.albacore.utils.UtilsBase;
+//
+//import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
+//import org.apache.http.impl.nio.client.HttpAsyncClients;
+//import org.apache.http.impl.nio.conn.PoolingNHttpClientConnectionManager;
+//import org.apache.http.impl.nio.reactor.DefaultConnectingIOReactor;
+//import org.apache.http.nio.conn.NHttpClientConnectionManager;
+//import org.apache.http.nio.reactor.IOReactorException;
+//
+//public class HTTPAsyncUtils extends UtilsBase {
+//	private static NHttpClientConnectionManager asyncman;
+//	static {
+//		try {
+//			asyncman = new PoolingNHttpClientConnectionManager(new DefaultConnectingIOReactor());
+//		} catch (IOReactorException e) {
+//			throw new SystemException("", e);
+//		}
+//	}
+//
+//	public static CloseableHttpAsyncClient create() {
+//		return HttpAsyncClients.createMinimal(asyncman);
+//	}
+//}
