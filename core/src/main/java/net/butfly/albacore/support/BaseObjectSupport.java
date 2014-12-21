fba2f713e2811db798b4f06b00bea42ab48b4700
कलविङ्क <zhangx@hzcominfo.com>
2014-12-21 17:52:23 +0000

    add

---
+++ core/src/main/java/net/butfly/albacore/support/BaseObjectSupport.java
@@ -0,0 +1,18 @@
+package net.butfly.albacore.support;
+
+public abstract class BaseObjectSupport<T extends ObjectSupport<T>> implements ObjectSupport<T> {
+	private static final long serialVersionUID = -2877820654223090498L;
+
+	@Override
+	final public int compareTo(T object) {
+		return this.equals(object) ? 0 : 1;
+	}
+
+	public Object shadowClone() {
+		try {
+			return super.clone();
+		} catch (CloneNotSupportedException e) {
+			return this;
+		}
+	}
+}
