8653d146d30f9a7a130a8dcff967135f9a12ccb9
कलविङ्क <zhangx@hzcominfo.com>
2015-01-07 18:43:50 +0000

    move to extra: pingying, swift.

---
+++ extra/src/main/java/net/butfly/albacore/helper/swift/exception/SwiftException.java
@@ -0,0 +1,24 @@
+package net.butfly.albacore.helper.swift.exception;
+
+import net.butfly.albacore.exception.BusinessException;
+
+public class SwiftException extends BusinessException {
+	private static final long serialVersionUID = 7587500560631008370L;
+	public static final String SWIFT_ERROR_CODE = "BUS_200";
+
+	public SwiftException() {
+		super(SWIFT_ERROR_CODE);
+	}
+
+	public SwiftException(String message) {
+		super(SWIFT_ERROR_CODE, message);
+	}
+
+	public SwiftException(Throwable cause) {
+		super(SWIFT_ERROR_CODE, cause);
+	}
+
+	public SwiftException(String message, Throwable cause) {
+		super(SWIFT_ERROR_CODE, message, cause);
+	}
+}
