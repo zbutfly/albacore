d4b6f51facf78fb581711fdde601934b35d7baf2
कलविङ्क <zhangx@hzcominfo.com>
2015-01-20 04:17:49 +0000

    fix filter callback mode.

--- core/src/main/java/net/butfly/albacore/utils/async/Tasks.java
+++ core/src/main/java/net/butfly/albacore/utils/async/Tasks.java
@@ -1,10 +1,12 @@
 package net.butfly.albacore.utils.async;
 
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import net.butfly.albacore.utils.Exceptions;
 import net.butfly.albacore.utils.UtilsBase;
@@ -44,15 +46,15 @@ final class Tasks extends UtilsBase {
 			}
 			consumer = executor.submit(new java.util.concurrent.Callable<T>() {
 				@Override
-				public T call() throws Exception {
+				public T call() {
 					return Tasks.callback(result, task.back);
 				}
 			});
 			if (task.options.unblock) return null;
 			try {
 				return consumer.get(task.options.timeout, TimeUnit.MILLISECONDS);
-			} catch (Exception ex) {
-				return handle(task, ex);
+			} catch (Exception ex) { // should not have internal exception on callback
+				throw Exceptions.unwrap(ex);
 			}
 		case LISTEN:
 			final Future<T> producer = executor.submit(task.call);
@@ -73,9 +75,10 @@ final class Tasks extends UtilsBase {
 			} catch (Exception ex) {
 				return handle(task, ex);
 			}
-			return null;
+			else return null;
+		default:
+			throw new IllegalArgumentException();
 		}
-		throw new IllegalArgumentException();
 	}
 
 	private static <OUT> OUT handle(Task<OUT> task, Exception ex) throws Exception {
@@ -89,7 +92,8 @@ final class Tasks extends UtilsBase {
 		return null;
 	}
 
-	private static <OUT> OUT fetch(Future<OUT> future, long timeout) throws Exception {
+	private static <OUT> OUT fetch(Future<OUT> future, long timeout) throws InterruptedException, ExecutionException,
+			TimeoutException {
 		try {
 			return timeout > 0 ? future.get(timeout, TimeUnit.MILLISECONDS) : future.get();
 		} catch (Exception e) {
