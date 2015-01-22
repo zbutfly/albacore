e8f7466ef3df3121b21e7a1fabcb99b1f6df4366
कलविङ्क <zhangx@hzcominfo.com>
2015-01-22 16:06:10 +0000

    ready for continuous mode.

--- core/src/main/java/net/butfly/albacore/utils/async/Options.java
+++ core/src/main/java/net/butfly/albacore/utils/async/Options.java
@@ -15,8 +15,8 @@ public class Options {
 	boolean unblock = ForkMode.NONE.async;
 
 	/**
-	 * Forking producer (Callable call) or consumer (Callback routine), another
-	 * will continue in current thread.
+	 * Forking producer (Callable call) or consumer (Callback routine), another will continue in current
+	 * thread.
 	 * 
 	 * @param producer
 	 *            True to fork producer thread, False to fork consumer thread.
@@ -27,8 +27,7 @@ public class Options {
 	}
 
 	/**
-	 * Forking both producer and consumer to a new thread, and current thread is
-	 * continuing immediately.
+	 * Forking both producer and consumer to a new thread, and current thread is continuing immediately.
 	 */
 	public Options fork() {
 		return this.mode(ForkMode.LISTEN);
@@ -56,6 +55,43 @@ public class Options {
 	private Options mode(ForkMode mode) {
 		this.mode = mode;
 		this.unblock = mode.async;
+		return this;
+	}
+
+	/* continuous options */
+
+	private static final int RETRIES_MAX = 100;
+	// default no continuous, < 0 for infinity
+	int repeat = 0;
+	// default no retry, finish on any failure, < 0 for MAX RETRIES
+	int retry = 0;
+	// default start in same thread
+	int concurrence = 0;
+	// default no waiting interval
+	long interval = 0;
+
+	public Options continuous(int repeat) {
+		this.repeat = repeat;
+		return this;
+	}
+
+	public Options continuous() {
+		this.repeat = -1;
+		return this;
+	}
+
+	public Options retries(int retries) {
+		this.retry = retries <= 0 || retries > RETRIES_MAX ? RETRIES_MAX : retries;
+		return this;
+	}
+
+	public Options concurrence(int concurrence) {
+		this.concurrence = concurrence;
+		return this;
+	}
+
+	public Options interval(long milliseconds) {
+		this.interval = milliseconds;
 		return this;
 	}
 }
