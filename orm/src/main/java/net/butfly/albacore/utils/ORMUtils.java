fd1c6665efafc6d557f8388e724cc0cfa26c2d72
कलविङ्क <zhangx@hzcominfo.com>
2015-01-30 19:00:23 +0000

    fix compile failure in constrict.

--- orm/src/main/java/net/butfly/albacore/utils/ORMUtils.java
+++ orm/src/main/java/net/butfly/albacore/utils/ORMUtils.java
@@ -8,10 +8,10 @@ import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 public final class ORMUtils extends UtilsBase {
 	private ORMUtils() {}
 
-	@SuppressWarnings("unchecked")
 	static public AbstractDataSource createDatasource(String driverClass, String url, String username, String password) {
 		SimpleDriverDataSource ds = new SimpleDriverDataSource();
-		ds.setDriverClass((Class<? extends Driver>) Reflections.forClassName(driverClass));
+		Class<? extends Driver> cl = Reflections.forClassName(driverClass);
+		ds.setDriverClass(cl);
 		ds.setUrl(url);
 		ds.setUsername(username);
 		ds.setPassword(password);
