c937c4f2620520acd1a10c8ed851ef8d48a4559f
कलविङ्क <zhangx@hzcominfo.com>
2015-02-05 17:01:14 +0000

    great performance: use same array pos instead of new array copy...

--- core/src/main/java/net/butfly/albacore/utils/Instances.java
+++ core/src/main/java/net/butfly/albacore/utils/Instances.java
@@ -1,6 +1,5 @@
 package net.butfly.albacore.utils;
 
-import java.util.Arrays;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -11,29 +10,25 @@ import java.util.concurrent.ConcurrentHashMap;
  *
  */
 public class Instances extends Utils {
-	private static final Map<Object, Object> INSTANCE_POOL = new InstancePool();
+	private static final Map<Object, Object> INSTANCE_POOL = new ConcurrentHashMap<Object, Object>();
 
 	public interface Instantiator<T> {
 		T create();
 	}
 
 	public static <T> T fetch(Instantiator<T> instantiator, Object... instanceKey) {
-		return fetchFrom(instantiator, INSTANCE_POOL, instantiator.getClass(), instanceKey);
+		return fetchFrom(instantiator, INSTANCE_POOL, instantiator.getClass(), instanceKey, 0);
 	}
 
 	@SuppressWarnings("unchecked")
-	private static <T> T fetchFrom(Instantiator<T> instantiator, Map<Object, Object> pool, Object currKey, Object[] otherKeys) {
-		boolean bottom = otherKeys == null || otherKeys.length == 0;
+	private static <T> T fetchFrom(Instantiator<T> instantiator, Map<Object, Object> pool, Object currKey, Object[] otherKeys,
+			int pos) {
+		boolean bottom = otherKeys.length <= pos;
 		Object result = pool.get(currKey);
 		if (null == result) {
-			result = bottom ? instantiator.create() : new InstancePool();
+			result = bottom ? instantiator.create() : new ConcurrentHashMap<Object, Object>();
 			if (null != result) pool.put(currKey, result);
 		}
-		return bottom ? (T) result : fetchFrom(instantiator, (Map<Object, Object>) result, otherKeys[0],
-				Arrays.copyOfRange(otherKeys, 1, otherKeys.length));
-	}
-
-	private final static class InstancePool extends ConcurrentHashMap<Object, Object> {
-		private static final long serialVersionUID = -7047645616308974050L;
+		return bottom ? (T) result : fetchFrom(instantiator, (Map<Object, Object>) result, otherKeys[pos], otherKeys, pos + 1);
 	}
 }
