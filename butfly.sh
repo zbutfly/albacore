e11ea5d1618478a702039add9713b2e44d0ddc11
कलविङ्क <zhangx@hzcominfo.com>
2015-07-19 09:00:43 +0000

    parents for 3.0.0-SNAPSHOT

--- butfly.sh
+++ butfly.sh
@@ -1,4 +1,5 @@
-## for release
-mvn deploy:deploy-file -DpomFile=butfly.pom -Dfile=butfly.pom -Durl=http://repos.corp.butfly.co:60080/nexus/content/repositories/releases/
-## for snapshot
-mvn deploy:deploy-file -DpomFile=butfly.pom -Dfile=butfly.pom -Durl=http://repos.corp.butfly.co:60080/nexus/content/repositories/snapshots/
+#!/bin/bash
+
+mvn -f butfly.pom deploy:deploy-file -DpomFile=butfly.pom -Dfile=butfly.pom -Durl=http://repos.corp.butfly.co:60080/nexus/content/repositories/releases/
+
+#mvn deploy:deploy-file -DpomFile=butfly.pom -Dfile=butfly.pom -Durl=http://repos.corp.butfly.co:60080/nexus/content/repositories/snapshots/
