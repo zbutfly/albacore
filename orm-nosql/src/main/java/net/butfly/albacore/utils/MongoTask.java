094aadd5cd4ec6b8a392833f860112c0fa40ab4b
कलविङ्क <zhangx@hzcominfo.com>
2015-07-19 09:00:08 +0000

    upgrade albacore/albus to 3.0.0 after tag 2.0.4-SNAPSHOT. add MongoTask for raw op. upgrade to JDK 1.8 at all.

---
+++ orm-nosql/src/main/java/net/butfly/albacore/utils/MongoTask.java
@@ -0,0 +1,45 @@
+package net.butfly.albacore.utils;
+
+import com.mongodb.ConnectionString;
+
+import net.butfly.albacore.utils.async.Task;
+
+public abstract class MongoTask {
+	private ConnectionString conn;
+
+	public MongoTask(ConnectionString conn) {
+		this.conn = conn;
+	}
+
+	abstract protected void executeAsync(com.mongodb.async.client.MongoDatabase db);
+
+	abstract protected void execute(com.mongodb.client.MongoDatabase db);
+
+	public void execute() {
+		this.execute(Instances.fetch(new Task.Callable<com.mongodb.MongoClient>() {
+			@Override
+			public com.mongodb.MongoClient call() throws Exception {
+				return new com.mongodb.MongoClient(new com.mongodb.MongoClientURI(MongoTask.this.conn.toString()));
+			}
+		}, MongoTask.this.conn).getDatabase(MongoTask.this.conn.getDatabase()));
+	}
+
+	public void executeAsync() {
+		this.executeAsync(Instances.fetch(new Task.Callable<com.mongodb.async.client.MongoClient>() {
+			@Override
+			public com.mongodb.async.client.MongoClient call() throws Exception {
+				return com.mongodb.async.client.MongoClients.create(MongoTask.this.conn);
+			}
+		}, MongoTask.this.conn).getDatabase(MongoTask.this.conn.getDatabase()));
+	}
+
+	public static ConnectionString buildConnectionString(String host, int port, String db, String username, String password) {
+		// mongodb://[username:password@]host1[:port1][,host2[:port2],...[,hostN[:portN]]][/[database][?options]]
+		StringBuilder sb = new StringBuilder("mongodb://");
+		if (!Texts.isEmpty(username) && !Texts.isEmpty(password)) sb.append(username).append(":").append("@");
+		sb.append(host);
+		if (port >= 0) sb.append(":").append(port);
+		if (!Texts.isEmpty(db)) sb.append("/").append(db);
+		return new ConnectionString(sb.toString());
+	}
+}
