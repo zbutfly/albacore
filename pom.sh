8b90fe3b514df1414baaa12baf77e66cd83d3692
कलविङ्क <zhangx@hzcominfo.com>
2016-02-19 10:24:31 +0000

    refactor

--- pom.sh
+++ pom.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mvn -f parent.pom deploy:deploy-file -DpomFile=parent.pom -Dfile=parent.pom -Durl=http://repos.corp.butfly.co:60080/nexus/content/repositories/releases/
+mvn -s C:\Users\butfly\.m2\settings-local.xml -f parent.pom deploy:deploy-file -DpomFile=parent.pom -Dfile=parent.pom -Durl=http://repos.corp.hzcominfo.com:6080/nexus/content/repositories/snapshots/
