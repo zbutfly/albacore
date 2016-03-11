26b30f1e55a840759134bdc57e838c5149682e50
कलविङ्क <zhangx@hzcominfo.com>
2016-03-11 17:40:45 +0800

    rebuild parent poms and construct calcs plugins

---
+++ calc/calculus.cmd
@@ -0,0 +1,1 @@
+mvnc -f calculus.pom deploy:deploy-file -DpomFile=calculus.pom -Dfile=calculus.pom -DrepositoryId=nexus -Durl=http://repos.corp.hzcominfo.com:6080/nexus/content/repositories/snapshots 
