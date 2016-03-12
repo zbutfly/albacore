85b759bf6a755d7ae6e2b44cf09284ce4b271179
कलविङ्क <zhangx@hzcominfo.com>
2016-03-13 03:23:53 +0800

    enable marshaller customizing in functor

--- calc/core/src/main/java/net/butfly/albacore/calculus/marshall/Marshaller.java
+++ calc/core/src/main/java/net/butfly/albacore/calculus/marshall/Marshaller.java
@@ -3,6 +3,7 @@ package net.butfly.albacore.calculus.marshall;
 import java.io.Serializable;
 
 import net.butfly.albacore.calculus.Functor;
+import net.butfly.albacore.calculus.Functor.Stocking;
 import net.butfly.albacore.calculus.FunctorConfig.Detail;
 import net.butfly.albacore.calculus.datasource.DataSource;
 
@@ -16,4 +17,15 @@ public interface Marshaller<V, K> extends Serializable {
 	<T extends Functor<T>> V marshall(T from);
 
 	<F extends Functor<F>> void confirm(Class<F> functorClass, DataSource ds, Detail detail);
+
+	static Marshaller<?, ?> parseMarshaller(Class<? extends Functor<?>> functorClass, DataSource ds) {
+		Stocking s = functorClass.getAnnotation(Stocking.class);
+		if (null == s) return null;
+		Class<? extends Marshaller<?, ?>> mc = s.marshaller();
+		try {
+			return DefaultMarshaller.class.equals(mc) ? ds.getMarshaller() : mc.newInstance();
+		} catch (InstantiationException | IllegalAccessException e) {
+			throw new RuntimeException(e);
+		}
+	}
 }
