241e77c609960564a969563bb4da9a1e02bb6bc9
कलविङ्क <zhangx@hzcominfo.com>
2016-07-22 10:38:04 +0800

    use mapPartitions in dataframe

--- calc/core/src/main/java/net/butfly/albacore/calculus/datasource/HiveDataSource.java
+++ calc/core/src/main/java/net/butfly/albacore/calculus/datasource/HiveDataSource.java
@@ -1,7 +1,11 @@
 package net.butfly.albacore.calculus.datasource;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.rdd.RDD;
 import org.apache.spark.sql.Column;
 import org.apache.spark.sql.DataFrame;
 import org.apache.spark.sql.Row;
@@ -13,8 +17,12 @@ import net.butfly.albacore.calculus.factor.Factor.Type;
 import net.butfly.albacore.calculus.factor.filter.FactorFilter;
 import net.butfly.albacore.calculus.factor.filter.HiveBuilder;
 import net.butfly.albacore.calculus.factor.modifier.Key;
+import net.butfly.albacore.calculus.factor.rds.internal.RDSupport;
 import net.butfly.albacore.calculus.marshall.RowMarshaller;
 import scala.Tuple2;
+import scala.collection.Iterator;
+import scala.collection.JavaConversions;
+import scala.runtime.AbstractFunction1;
 
 public class HiveDataSource extends DataSource<Object, Row, Row, Object, Row> {
 	private static final long serialVersionUID = 2229814193461610013L;
@@ -49,6 +57,19 @@ public class HiveDataSource extends DataSource<Object, Row, Row, Object, Row> {
 		String key = ((RowMarshaller) marshaller).parseQualifier(factor, marshaller.parse(factor, Key.class)._1, df.schema().fieldNames());
 		df = df.repartition(new Column(key));
 		if (expandPartitions > 1) df = df.repartition((int) Math.ceil(df.javaRDD().getNumPartitions() * expandPartitions));
-		return df.javaRDD().mapToPair(row -> new Tuple2<Object, F>(row.get(row.fieldIndex(key)), marshaller.unmarshall(row, factor)));
+		RDD<Tuple2<Object, F>> rdd = df.mapPartitions(new AbstractFunction1<Iterator<Row>, Iterator<Tuple2<Object, F>>>() {
+			@Override
+			public Iterator<Tuple2<Object, F>> apply(Iterator<Row> rows) {
+				List<Tuple2<Object, F>> list = new ArrayList<>();
+				while (rows.hasNext()) {
+					Row r = rows.next();
+					list.add(new Tuple2<Object, F>(r.get(r.fieldIndex(key)), marshaller.unmarshall(r, factor)));
+
+				}
+				return JavaConversions.asScalaIterator(list.iterator());
+			}
+		}, RDSupport.tag());
+		return rdd.toJavaRDD().mapToPair(t -> t);
 	}
+
 }
