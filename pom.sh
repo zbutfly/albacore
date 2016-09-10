fef0a0ce4e35df88942b647155e10f55295925e7
कलविङ्क <zhangx@hzcominfo.com>
2016-09-11 00:33:05 +0800

    enable current version for compile

--- pom.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mvn -f butfly.pom deploy:deploy-file -DpomFile=butfly.pom -Dfile=butfly.pom -Durl=http://repos.corp.butfly.co:60080/nexus/content/repositories/releases/
-
-#mvn deploy:deploy-file -DpomFile=butfly.pom -Dfile=butfly.pom -Durl=http://repos.corp.butfly.co:60080/nexus/content/repositories/snapshots/
-
