d245073c153a7eae06a2555002a7dd15fcb876d3
कलविङ्क <zhangx@hzcominfo.com>
2016-09-24 01:09:37 +0800

    more general i/o

--- core/src/main/java/net/butfly/albacore/io/Input.java
+++ core/src/main/java/net/butfly/albacore/io/Input.java
@@ -4,16 +4,9 @@ import java.io.Closeable;
 import java.io.IOException;
 import java.io.Serializable;
 import java.util.List;
-import java.util.Map;
 
-import scala.Tuple3;
-
-@SuppressWarnings("unchecked")
-public interface Input<F> extends Serializable, Closeable {
-
-	List<Tuple3<String, byte[], Map<String, Object>>> read(F... filters);
-
-	<E> List<Tuple3<String, byte[], E>> read(Class<E> cl, F... filters);
+public interface Input<D> extends Serializable, Closeable {
+	List<D> read();
 
 	void commit();
 
--- core/src/main/java/net/butfly/albacore/io/Output.java
+++ core/src/main/java/net/butfly/albacore/io/Output.java
@@ -3,13 +3,10 @@ package net.butfly.albacore.io;
 import java.io.Closeable;
 import java.io.IOException;
 import java.io.Serializable;
-import java.util.Map;
 
-@SuppressWarnings("unchecked")
-public interface Output<F> extends Serializable, Closeable {
-	void write(F dest, Map<String, Object>... data);
-
-	<E> void write(F dest, E... data);
+public interface Output<D> extends Serializable, Closeable {
+	@SuppressWarnings("unchecked")
+	void write(D... data);
 
 	@Override
 	default void close() throws IOException {}
