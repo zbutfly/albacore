836d1ffaac44e4c5f1f9af6f43b130128d5614cc
कलविङ्क <zhangx@hzcominfo.com>
2016-09-28 00:17:02 +0800

    optimize dependencies version

--- serializer/hbase/src/main/java/net/butfly/albacore/serder/HbaseValueSerder.java
+++ serializer/hbase/src/main/java/net/butfly/albacore/serder/HbaseValueSerder.java
@@ -1,20 +1,30 @@
 package net.butfly.albacore.serder;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
 
 import com.google.common.reflect.TypeToken;
 
-public class HbaseValueSerder implements Serder<byte[], ImmutableBytesWritable> {
+public class HbaseValueSerder implements Serder<ByteArrayOutputStream, ImmutableBytesWritable> {
 	private static final long serialVersionUID = 1152380944308233135L;
 
 	@Override
-	public <T> ImmutableBytesWritable ser(T from) {
-		return null == from ? null : new ImmutableBytesWritable((byte[]) from);
+	public <T extends ByteArrayOutputStream> ImmutableBytesWritable ser(T from) {
+		return null == from ? null : new ImmutableBytesWritable(from.toByteArray());
 	}
 
 	@SuppressWarnings("unchecked")
 	@Override
-	public <T> T der(ImmutableBytesWritable from, TypeToken<T> to) {
-		return null == from ? null : (T) from.get();
+	public <T extends ByteArrayOutputStream> T der(ImmutableBytesWritable from, TypeToken<T> to) {
+		if (null == from) return null;
+		ByteArrayOutputStream bao = new ByteArrayOutputStream();
+		try {
+			bao.write(from.get());
+		} catch (IOException e) {
+			return null;
+		}
+		return (T) bao;
 	}
 }
