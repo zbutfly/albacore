8450ea1b89857e8d549afa5b3e3c5566752e0b84
कलविङ्क <zhangx@hzcominfo.com>
2017-01-11 15:22:59 +0800

    use single open pump

--- core/src/main/java/net/butfly/albacore/io/pump/Pump.java
+++ core/src/main/java/net/butfly/albacore/io/pump/Pump.java
@@ -1,7 +1,5 @@
 package net.butfly.albacore.io.pump;
 
-import java.util.concurrent.atomic.AtomicBoolean;
-
 import net.butfly.albacore.io.Openable;
 import net.butfly.albacore.io.stats.Statistical;
 import net.butfly.albacore.utils.Systems;
@@ -9,18 +7,21 @@ import net.butfly.albacore.utils.logger.Logger;
 
 public interface Pump extends Statistical<Pump>, Openable {
 	final static Logger logger = Logger.getLogger(Pump.class);
-	static final AtomicBoolean SIGNAL_HANDLED = new AtomicBoolean(false);
 
 	Pump batch(long batchSize);
 
 	void terminate();
 
-	static void run(Pump... pumps) {
+	@Override
+	default void opening() {
 		// handle kill -15, CTRL-C, kill -9
-		if (!SIGNAL_HANDLED.getAndSet(true)) Systems.handleSignal(sig -> {
-			for (int i = 0; i < pumps.length; i++)
-				((PumpImpl) pumps[i]).terminate();
-		}, "TERM", "INT"/* , "KILL" */); // kill -9 catched by system/os
+		Systems.handleSignal(sig -> terminate(), "TERM", "INT");
+		/* , "KILL" */
+		// kill -9 catched by system/os
+	}
+
+	@Deprecated
+	static void run(Pump... pumps) {
 		for (int i = pumps.length - 1; i >= 0; i--)
 			((PumpImpl) pumps[i]).open();
 		for (int i = 0; i < pumps.length; i++)
