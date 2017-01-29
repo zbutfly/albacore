8944645a3245dd3d2a7fe5e0b94e60f8f172b492
कलविङ्क <zhangx@hzcominfo.com>
2017-01-29 15:18:12 +0800

    bug in invocationhandler

--- core/src/main/java/net/butfly/albacore/lambda/InvocationHandler.java
+++ core/src/main/java/net/butfly/albacore/lambda/InvocationHandler.java
@@ -6,10 +6,10 @@ import java.lang.reflect.Proxy;
 @FunctionalInterface
 public interface InvocationHandler extends java.lang.reflect.InvocationHandler {
 	static <T> T proxy(InvocationHandler handler, Class<? super T>... interfaces) {
-		return (T) Proxy.newProxyInstance(Object.class.getClassLoader(), interfaces, handler);
+		return (T) Proxy.newProxyInstance(Thread.currentThread().getContextClassLoader(), interfaces, handler);
 	}
 
 	static <T> T proxy(InvocationHandler handler, Class<? super T> intf) {
-		return (T) Proxy.newProxyInstance(Object.class.getClassLoader(), new Class<?>[] { intf }, handler);
+		return (T) Proxy.newProxyInstance(Thread.currentThread().getContextClassLoader(), new Class<?>[] { intf }, handler);
 	}
 }
