2fada9b95097f923b12f7d80948fbea61a8826da
कलविङ्क <zhangx@hzcominfo.com>
2017-02-04 23:29:25 +0800

    add map props

--- core/src/main/java/net/butfly/albacore/utils/collection/Maps.java
+++ core/src/main/java/net/butfly/albacore/utils/collection/Maps.java
@@ -1,14 +1,23 @@
 package net.butfly.albacore.utils.collection;
 
-import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Maps {
 	@SuppressWarnings("unchecked")
 	public static <K, V> Map<K, V> of(Object... kvs) {
-		Map<K, V> map = new HashMap<>();
+		Map<K, V> map = new ConcurrentHashMap<>();
 		for (int i = 0; i + 1 < kvs.length; i += 2)
 			map.put((K) kvs[i], (V) kvs[i + 1]);
+		return map;
+	}
+
+	public static Map<String, String> of(Properties props) {
+		Map<String, String> map = new ConcurrentHashMap<>();
+		props.entrySet().parallelStream().forEach(e -> {
+			if (e.getValue() != null) map.put(e.getKey().toString(), e.getValue().toString());
+		});
 		return map;
 	}
 }
