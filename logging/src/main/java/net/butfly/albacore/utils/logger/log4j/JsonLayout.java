2c142e730ef1efa1dbe246d1b559f89f65f8f6ee
कलविङ्क <zhangx@hzcominfo.com>
2019-08-21 21:45:26 +0800

    move json layout impl into perf stats

--- logging/src/main/java/net/butfly/albacore/utils/logger/log4j/JsonLayout.java
+++ logging/src/main/java/net/butfly/albacore/utils/logger/log4j/JsonLayout.java
@@ -1,17 +1,7 @@
 package net.butfly.albacore.utils.logger.log4j;
 
-import java.util.Date;
-import java.util.Hashtable;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-
 import org.apache.log4j.Layout;
-import org.apache.log4j.MDC;
 import org.apache.log4j.spi.LoggingEvent;
-import org.apache.log4j.spi.ThrowableInformation;
-
-import net.butfly.albacore.utils.Texts;
-import net.butfly.albacore.utils.json.Jsonx;
 
 public class JsonLayout extends Layout {
 	private boolean debug = false;// stacktrack outputting
@@ -31,24 +21,25 @@ public class JsonLayout extends Layout {
 	@Override
 	public void activateOptions() {}
 
-	@SuppressWarnings({ "rawtypes", "unchecked" })
+//	@SuppressWarnings({ "rawtypes", "unchecked" })
 	@Override
 	public String format(LoggingEvent event) {
-		Map<String, Object> v = new ConcurrentHashMap<>();
-		Hashtable mdc = MDC.getContext();
-		if (null != mdc) mdc.forEach((k, vv) -> {
-			if (null != vv) v.put(k.toString(), vv);
-		});
-		if (!v.containsKey("STATS_TABLE")) return "";
-		v.put("OP_TIME", Texts.formatDate(timeFormat, new Date(event.timeStamp)));
-		ThrowableInformation thi = event.getThrowableInformation();
-		if (null != thi) {
-			if (null != thi.getThrowable().getMessage()) v.put("ERR_MESSAGE", thi.getThrowable().getMessage());
-			if (debug && null != thi.getThrowableStrRep()) v.put("ERR_STACKTRACE", thi.getThrowableStrRep());
-		}
+		// Map<String, Object> v = new ConcurrentHashMap<>();
+		// Hashtable mdc = MDC.getContext();
+		// if (null != mdc) mdc.forEach((k, vv) -> {
+		// if (null != vv) v.put(k.toString(), vv);
+		// });
+		// if (!v.containsKey("STATS_TABLE")) return "";
+		// v.put("OP_TIME", Texts.formatDate(timeFormat, new Date(event.timeStamp)));
+		// ThrowableInformation thi = event.getThrowableInformation();
+		// if (null != thi) {
+		// if (null != thi.getThrowable().getMessage()) v.put("ERR_MESSAGE", thi.getThrowable().getMessage());
+		// if (debug && null != thi.getThrowableStrRep()) v.put("ERR_STACKTRACE", thi.getThrowableStrRep());
+		// }
 		Object m = event.getMessage();
-		if (null != m) v.put("LOG_MESSAGE", m);
-		return Jsonx.json(v) + "\n";
+		// if (null != m) v.put("LOG_MESSAGE", m);
+		// return Jsonx.json(v) + "\n";
+		return null == m ? "" : m.toString();
 	}
 
 	@Override
