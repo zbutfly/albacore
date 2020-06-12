6090bb1b0db8dc98b7954e0d3abf0a38fc32ef90
कलविङ्क <zhangx@hzcominfo.com>
2020-06-12 20:42:27 +0800

    add message for not implementation

--- core/src/main/java/net/butfly/albacore/exception/NotImplementedException.java
+++ core/src/main/java/net/butfly/albacore/exception/NotImplementedException.java
@@ -2,4 +2,12 @@ package net.butfly.albacore.exception;
 
 public class NotImplementedException extends RuntimeException {
 	private static final long serialVersionUID = -3529528761459501530L;
+
+	public NotImplementedException() {
+		super();
+	}
+
+	public NotImplementedException(String message) {
+		super(message);
+	}
 }
